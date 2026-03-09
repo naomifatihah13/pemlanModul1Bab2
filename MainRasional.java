60f0907a377a13c4cfd45e86906f6a4fe8510cd2
naomifatihah13 <naomi.anamika.fatihah@gmail.com>
2026-03-09 22:51:03 +0700

    pemlan dua

---
+++ MainRasional.java
@@ -0,0 +1,61 @@
+public class MainRasional {
+    public static void main(String[] args) {
+        Rasional R1 = new Rasional (1,2);
+        Rasional R2 = new Rasional (1, 3);
+
+        System.out.println("R1.isRasional: " + R1.isRasional());
+        System.out.println("R2.isRasional: " + R2.isRasional());
+        System.out.println();
+
+        System.out.println("R1 > R2 : " + R1.moreThan(R2));
+        System.out.println("R1 < R2 : " + R1.lessThan(R2));
+        System.out.println("R1 >= R2 : " + R1.moreThanOrEqual(R2));
+        System.out.println("R1 <= R2 : " + R1.lessThanOrEqual(R2));
+        System.out.println();
+
+        System.out.println("R1 : ");
+        R1.cetak();
+        System.out.println("R2 : ");
+        R2.cetak();
+        System.out.println();
+
+        R1.Sederhana();
+        R2.Sederhana();
+
+        System.out.println("R1 : ");
+        R1.cetak();
+        System.out.println("R2 : ");
+        R2.cetak();
+        System.out.println();
+
+        System.out.println("Setelah dilakukan Cast ke double menjadi : ");
+        System.out.println("R1 : " + R1.Cast());
+        System.out.println("R2 : " + R2.Cast());
+        System.out.println();
+        
+        R1.negasi();
+        System.out.println("Unary- dari R1 :");
+        R1.cetak();
+        System.out.println();
+        
+        R1.unaryPlus(R2);
+        System.out.println("Nilai dari 'R1 += R2' : ");
+        R1.cetak();
+        System.out.println();
+
+        R1.unaryMinus(R2);
+        System.out.println("Nilai dari 'R1 -= R2' : ");
+        R1.cetak();
+        System.out.println();
+
+        R1.unaryKali(R2);
+        System.out.println("Nilai dari 'R1 *= R2' : ");
+        R1.cetak();
+        System.out.println();
+
+        R1.unaryBagi(R2);
+        System.out.println("Nilai dari 'R1 /= R2' : ");
+        R1.cetak();
+        System.out.println();
+    }
+}
---
+++ Rasional.java
@@ -0,0 +1,87 @@
+public class Rasional {
+    private int pembilang, penyebut;
+    
+    public Rasional() {
+        pembilang = 0;
+        penyebut = 1;
+    }
+
+    public Rasional(int pbl, int pyb) {
+        pembilang = pbl;
+        penyebut = pyb;
+    }
+
+    //mengecek suatu bilangan adalah rasional atau bukan
+    public boolean isRasional() {
+        return (penyebut != 0);
+    }
+
+    //menyederhanakan bilangan rasional
+    public void Sederhana() {
+        int temp, A, B;
+        if (penyebut == 0) {
+            return;
+        }
+        A = (pembilang < penyebut) ? penyebut:pembilang;
+        B = (pembilang < penyebut) ? pembilang:penyebut;
+        for (; B != 0; ) {
+            temp = A % B;
+            A = B;
+            B = temp;
+        }
+
+        pembilang /=A;
+        penyebut /=A;
+    }
+
+    public double Cast() {
+        return (penyebut == 0) ? 0.0 : (double)pembilang/(double)penyebut;
+    }
+
+    //oprator
+    public boolean moreThan (Rasional A) {
+        return (pembilang * A.penyebut > penyebut * A.pembilang);
+    }
+
+    public boolean lessThan (Rasional A) {
+        return (pembilang * A.penyebut < penyebut * A.pembilang);
+    }
+
+    public boolean lessThanOrEqual (Rasional A) {
+        return (pembilang * A.penyebut <= penyebut * A.pembilang);
+    }
+
+    public boolean moreThanOrEqual (Rasional A) {
+        return (pembilang * A.penyebut >= penyebut * A.pembilang);
+    }
+
+    //operator Unary- ---> A = -A
+    public void negasi() {
+        pembilang = - pembilang;
+    }
+
+    //operator unary += \
+    public void unaryPlus (Rasional A) {
+        pembilang = pembilang * A.penyebut + penyebut * A.pembilang;
+        penyebut *=A.penyebut;
+    }
+
+    public void unaryMinus (Rasional A) {
+        pembilang = pembilang * A.penyebut - penyebut * A.pembilang;
+        penyebut *=A.penyebut;
+    }
+
+    public void unaryKali (Rasional A) {
+        pembilang *=A.pembilang;
+        penyebut *=A.penyebut;
+    }
+
+    public void unaryBagi (Rasional A) {
+        pembilang *=A.penyebut;
+        penyebut *=A.pembilang;
+    }
+
+    public void cetak() {
+        System.out.println(pembilang + "/" + penyebut);
+    }
+}
